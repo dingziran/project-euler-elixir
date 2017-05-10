53c0dfd3bebf55ac3022fc31d6fb747a279833d3
dingziran <dingziranrr@163.com>
2017-05-10 18:25:25 +0800

    finish problem 2

---
+++ problem_2.exs
@@ -0,0 +1,10 @@
+defmodule Problem2 do
+def fib(x, y, acc) do
+  cond do
+    y>4_000_000 -> acc
+    rem(y,2)==0 -> fib(y, x+y, acc)
+    true -> fib(y, x+y, acc+y)
+  end
+end
+end
+Problem2.fib(1,2,1) |> IO.puts
