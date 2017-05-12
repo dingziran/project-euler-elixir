f643314bbb1c68269eaaa2624d04d5758df82822
dingziran <dingziranrr@163.com>
2017-05-12 22:28:06 +0800

    fix problem3's bug

--- problem_3.exs
+++ problem_3.exs
@@ -2,9 +2,15 @@ defmodule Problem3 do
   def prime(i, num, last) do
     cond do
       i>num -> last
-      rem(num, i)==0 -> prime(i+1, div(num,i), i)
+      rem(num, i)==0 -> prime(i+1, deepdiv(num,i), i)
       true -> prime(i+1, num, last)
     end
+  end
+  def deepdiv(num, i) when rem(num,i)==0 do
+     div(num, i)
+  end
+  def deepdiv(num, _) do
+     num
   end
 end
 
